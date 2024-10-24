78254e9b9256c5e61aa879d126f76819a9225bb8
Djangoboyy <romanshevtsovcoderman@yandex.ru>
2024-10-24 18:27:01 +0300

    after refactoring

--- REF1.cpp
+++ REF1.cpp
@@ -1,44 +1,47 @@
 ﻿#include <iostream>
-#include <cmath>
+#include <ctime>
 
-using namespace std;
+
+
+void series_filler(int* A, int size) {
+    for (int i = 0; i < size; i++)
+        A[i] = rand() % 10;
+}
+
+
+void series_print(int* A, int size) {
+    for (int i = 0; i < size; i++)
+        std::cout << A[i] << " ";
+    std::cout << std::endl;
+}
+
+
+int series_average(int* A, int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+        sum += A[i];
+
+    return sum / size;
+}
+
+
+
 int main()
 {
-    int i; float a[10], b[10], c[10];
-    float sa, sb, sc;
-    sa = 0;
-    sb = 0;
-    sc = 0;
-    cout << "pervaja posledovatelnost" << endl;
-    for (i = 0; i < 10; i++)
-    {
-        a[i] = rand() % 10;
-        cout << a[i];
-        cout << ' ';
-        sa = sa + a[i];
+
+    std::srand(time(0));
+
+    int arr_size = 10;
+
+
+    for( int count = 1; count <= 3; count++ ) {
+        int array[arr_size];
+        series_filler( array, arr_size );
+        std::cout << count << " последовательность" << std::endl;
+        series_print( array, arr_size );
+        std::cout << "Среднее " << count << " последовательности: " << series_average( array, arr_size ) << std::endl << std::endl;
     }
-    cout << ' ' << endl;
-    cout << "srednee pervoy posledovatelnocty =" << sa / 10 << endl;
-    cout << "vtoraja posledovatelnost" << endl;
-    for (i = 0; i < 10; i++)
-    {
-        b[i] = rand() % 10;
-        cout << b[i];
-        cout << ' ';
-        sb = sb + b[i];
-    }
-    cout << ' ' << endl;
-    cout << "srednee vtoroy posledovatelnocty =" << sb / 10 << endl;
-    cout << "tretja posledovatelnost" << endl;
-    for (i = 0; i < 10; i++)
-    {
-        c[i] = rand() % 10;
-        cout << c[i];
-        cout << ' ';
-        sc = sc + c[i];
-    }
-    cout << ' ' << endl;
-    cout << "srednee tretjey posledovatelnocty =" << sc / 10 << endl;
+
 
     return 0;
 }
