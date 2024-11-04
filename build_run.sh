e15d9b2400c70e4faffdc85a09351b4d72cf4a91
Djangoboyy <romanshevtsovcoderman@yandex.ru>
2024-11-04 22:52:49 +0300

    Изменена структура репозитория

---
+++ build_run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Установка кодировки
+CHARSET="-finput-charset=utf-8 -fexec-charset=utf-8"
+
+# Определение имен файлов
+MAIN="AFTER_REF.cpp"
+PRODUCT="AFTER_REF"
+
+# Удаление предыдущего исполняемого файла, если он существует
+if [ -f "$PRODUCT" ]; then
+    rm $PRODUCT
+fi
+
+# Компиляция программы
+g++ $CHARSET $MAIN -o $PRODUCT
+
+./$PRODUCT
